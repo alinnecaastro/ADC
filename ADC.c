871b0cddfc2c3071f7db0e8483d336022fd75f63
alinnecaastro <alinnecastro133@gmail.com>
2025-02-18 22:59:02 -0300

    adc

--- ADC.c
+++ ADC.c
@@ -15,28 +15,6 @@
 #include "pico/bootrom.h"
 #include "hardware/adc.h"
 
-#define JOYSTICK_X_PIN 26  // Pino ADC conectado ao eixo Y do joystick
-#define JOYSTICK_Y_PIN 27  // Pino ADC conectado ao eixo Y do joystick
-#define BUTTON_PIN 22
-#define LED_GREEN_PIN 11
-#define LED_BLUE_PIN 12   // Pino PWM conectado ao LED Azul
-#define LED_RED_PIN 13   // Pino PWM conectado ao LED Azul
-#define CENTER_VALUE 2048
-#define JOYSTICK_MAX 4095
-#define DEADZONE 110
-#define BUTTON_A_PIN  5
-
-
-// Definições para o display
-#define WIDTH 128
-#define HEIGHT 64
-#define SQUARE_SIZE 8
-#define I2C_SDA_PIN 14   // Pino SDA do I2C
-#define I2C_SCL_PIN 15     // Pino SCL do I2C
-#define I2C_PORT i2c1
-#define endereco 0x3C
-
-
 
 // Variáveis de estado
 bool led_green_state = false;       // Estado do LED Verde
